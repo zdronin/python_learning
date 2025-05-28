8d5cc2915880ae8a238578b2f2a0e5e263131cf5
Chidera Augustine || zdronin <146446271+zdronin@users.noreply.github.com>
2025-05-28 06:04:57 +0100

    Update Smart_Access_Control_System.py

--- projects/Smart_Access_Control_System.py
+++ projects/Smart_Access_Control_System.py
@@ -17,7 +17,7 @@ is_banned   = bool(input("Are you banned?True or False: "))       # True or Fals
 
 if is_employee  and has_badge:
     print("Access granted to employee")
-elif is_guest and is_approved_guest not is_banned:
+elif is_guest and is_approved_guest:
     print("Access granted to guest")
 else:
     print("Access denied")
